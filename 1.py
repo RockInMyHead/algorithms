a6792e820c66e4ca5443b5ff576bbd921c08cdd2
RockInMyHead <65908604+RockInMyHead@users.noreply.github.com>
2024-05-11 10:28:26 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sort_algorithm(arr):
+    return sorted(arr)
+
+def main_sort():
+    arr = [5, 2, 9, 1, 5, 6]
+    sorted_arr = sort_algorithm(arr)
+    print("Отсортированный массив:", sorted_arr)
+
+if __name__ == "__main__":
+    main_sort()
---
+++ 2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def define_main_diagonal_sum(matrix):
+    return np.trace(matrix)
+
+def define_secondary_diagonal_sum(matrix):
+    return np.trace(np.fliplr(matrix))
+
+def main_diagonal_sum():
+    matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print("Матрица:")
+    print(matrix)
+    print("Сумма элементов на главной диагонали:", define_main_diagonal_sum(matrix))
+    print("Сумма элементов на побочной диагонали:", define_secondary_diagonal_sum(matrix))
+
+if __name__ == "__main__":
+    main_diagonal_sum()
